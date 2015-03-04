c56655903d84f0322dd1bde4116fb4f712d4b681
cjq002 <692771080@qq.com>
2015-03-04 09:50:55 +0800

    Initial Commit

---
+++ UISwitch创建和使用/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UISwitch创建和使用
+//
+//  Created by 陈家庆 on 15-3-4.
+//  Copyright (c) 2015年 shikee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
